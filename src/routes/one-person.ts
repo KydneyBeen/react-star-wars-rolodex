3678a0c0738739d59d062c8b65412e6ef4922e07
Sydney Y <syoung.is@hotmail.com>
2021-12-12 00:27:27 -0800

    Make API source interchangeable

--- src/routes/one-person.ts
+++ src/routes/one-person.ts
@@ -14,7 +14,7 @@ const getPersonDetail = async (pathPart: string): Promise<PersonDetail | null> =
   if (peopleCache[pathPart]) {
     return peopleCache[pathPart];
   }
-  const person: Person = await apiRequest(Resource.people, pathPart);
+  const person: Person = await apiRequest(Resource.people, pathPart + '/');
   const personSpecies: Array<Species> = [];
   const personFilms: Array<Film> = [];
   let personHomeworld: Planet | null;
@@ -23,8 +23,8 @@ const getPersonDetail = async (pathPart: string): Promise<PersonDetail | null> =
       person.species.map(async (species) => {
         if (species && typeof species === 'string') {
           const parsedUrl = species.match(/[0-9]+/g);
-          const id: string = parsedUrl ? parsedUrl[0] : '0';
-          const speciesObj: Species = await apiRequest(Resource.species, id);
+          const id: string = parsedUrl ? parsedUrl[parsedUrl.length - 1] : '0';
+          const speciesObj: Species = await apiRequest(Resource.species, id + '/');
           personSpecies.push(speciesObj);
         }
       }),
@@ -37,8 +37,8 @@ const getPersonDetail = async (pathPart: string): Promise<PersonDetail | null> =
       person.films.map(async (film) => {
         if (film && typeof film === 'string') {
           const parsedUrl = film.match(/[0-9]+/g);
-          const id: string = parsedUrl ? parsedUrl[0] : '0';
-          const filmObj: Film = await apiRequest(Resource.films, id);
+          const id: string = parsedUrl ? parsedUrl[parsedUrl.length - 1] : '0';
+          const filmObj: Film = await apiRequest(Resource.films, id + '/');
           personFilms.push(filmObj);
         }
       }),
@@ -48,8 +48,8 @@ const getPersonDetail = async (pathPart: string): Promise<PersonDetail | null> =
   }
   if (person && person.homeworld && typeof person.homeworld === 'string') {
     const parsedUrl = person.homeworld.match(/[0-9]+/g);
-    const id: string = parsedUrl ? parsedUrl[0] : '0';
-    const planetObj: Planet = await apiRequest(Resource.planets, id);
+    const id: string = parsedUrl ? parsedUrl[parsedUrl.length - 1] : '0';
+    const planetObj: Planet = await apiRequest(Resource.planets, id + '/');
     personHomeworld = planetObj;
   } else {
     personHomeworld = null;
